2309b8467ad44e0d01dbc800c6bb4163652808cb
Selu <selu@macbook.local>
2013-07-13 14:18:39 +0200

    Add lita r2d2 robot

---
+++ lita_config.rb
@@ -0,0 +1,31 @@
+Lita.configure do |config|
+  # The name your robot will use.
+  config.robot.name = "R2D2"
+  config.robot.mention_name = "r2d2"
+
+  # The severity of messages to log. Options are:
+  # :debug, :info, :warn, :error, :fatal
+  # Messages at the selected level and above will be logged.
+  config.robot.log_level = :info
+
+  # An array of user IDs that are considered administrators. These users
+  # the ability to add and remove other users from authorization groups.
+  # What is considered a user ID will change depending on which adapter you use.
+  # config.robot.admins = ["1", "2"]
+
+  # The adapter you want to connect with. Make sure you've added the
+  # appropriate gem to the Gemfile.
+  config.robot.adapter = :hipchat
+
+  config.adapter.jid = ENV["HIPCHAT_JID"]
+  config.adapter.password = ENV["HIPCHAT_PASSWORD"]
+  config.adapter.debug = false
+  config.adapter.rooms = ENV["HIPCHAT_ROOMS"]
+  config.adapter.muc_domain = ENV["HIPCHAT_DOMAIN"]
+
+  config.redis.url = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["HTTP_PORT"]
+
+
+  config.handlers.google_images.safe_search = :off
+end
